831b36b02ecc075094540c06ca63c2e8c3711cea
Mendel Liang <mendel.liang@live.com.au>
2016-02-18 18:58:06 +1100

    Added music, gifs

--- MouseMove/resize.js
+++ MouseMove/resize.js
@@ -1,17 +1,20 @@
 (function() {
     var canvas = document.getElementById('canvas');
+    var cover = document.getElementById('cover');
     var ctx = canvas.getContext('2d');
     var mouse = {x:0, y:0};
     var imgpos = {x:0, y:0};
+    var img = new Image();
+    img.src = "defcon.png";
     var height = 128;
     var width = 128;
+    var onPic = false;
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
-        
+
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-                
                 /**
                  * Your drawings need to be inside this function otherwise they will be reset when 
                  * you resize the browser window and the canvas goes will be cleared.
@@ -19,7 +22,6 @@
         drawStuff(); 
     }
     resizeCanvas();
-        
     function drawStuff() {
         drawRandomSpot();
     }
@@ -27,6 +29,16 @@
     canvas.addEventListener('mousemove', function(e){
         mouse.x = e.pageX - this.offsetLeft;
         mouse.y = e.pageY - this.offsetTop;
+        if((mouse.x >= imgpos.x && mouse.x <= imgpos.x + width) && (mouse.y >= imgpos.y && mouse.y <= imgpos.y + height)){
+            if(onPic == false){
+                onPic = true;
+                $(cover).hide();
+            }
+        }
+        else{
+            onPic = false;
+            $(cover).show();
+        }
     }, false);
 
     canvas.addEventListener('click', function(){
@@ -39,12 +51,14 @@
     }, false);
 
     function drawRandomSpot(){
-        var image = document.getElementById("image");
-        var locWidth = getRandomInt(0, window.innerWidth - image.width*2);
-        var locHeight = getRandomInt(0, window.innerHeight - image.height*2);
-        ctx.drawImage(image, locWidth, locHeight, width, height);
-        imgpos.x = locWidth;
-        imgpos.y = locHeight;
+        
+        img.onload = function(){
+            var locWidth = getRandomInt(0, window.innerWidth - image.width*2);
+            var locHeight = getRandomInt(0, window.innerHeight - image.height*2);
+            imgpos.x = locWidth;
+            imgpos.y = locHeight;
+            ctx.drawImage(image, locWidth, locHeight, width, height);
+        }
     }
 
     function getRandomInt(min, max) {
--- colors.js
+++ colors.js
@@ -1,23 +1,34 @@
 var pulsateInterval = null, i = 0;
 $(document).ready(function() {
       // set interval to pulsate every 1500ms
+      myAudio = new Audio('moe.mp3'); 
+      if (typeof myAudio.loop == 'boolean'){
+       myAudio.loop = true;
+      }
+      else{
+        myAudio.addEventListener('ended', function() {
+          this.currentTime = 0;
+          this.play();
+        }, false);
+      } 
+      myAudio.play();
       pulsateInterval = setInterval(function(){
           // animate back and forth
           $('body').animate({
              backgroundColor: '#6600CC',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#0000CC',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#3300FF',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#9900CC',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#3300CC',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#006699',
-          }, 200).animate({
+          }, 545).animate({
              backgroundColor: '#330099',
-          }, 300);
+          }, 545);
           i++;
           // stop at 50000 pulsations
           if(i == 50000){
@@ -25,3 +36,13 @@ $(document).ready(function() {
           }
       }, 1600);
 });
+
+function changePenguin(){
+  $(".cat").hide();
+  $(".penguin").show();
+}
+
+function changeCat(){
+  $(".penguin").hide();
+  $(".cat").show();
+}
